d6011c70a63e70c56b44dedca309c55fb001d47b
Matthew Pagan <mastapegs01@gmail.com>
2023-09-24 09:42:52 -0400

    iterate on frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,15 +4,22 @@ function App() {
   return (
     <>
       <div className={styles.container}>
-        <h1>Hello Noah!</h1>
-        <p>I love you so much!</p>
-        <p>
-          I wanted to let you know that you'll be getting Robux every single
-          month for an entire year as part of your birthday present.
-        </p>
-        <p>Don't spend it all in one obby!</p>
-        <p>Love,</p>
-        <p>Mommy and Daddy</p>
+        <div className={styles.mainContainer}>
+          <h1>Hello Noah!</h1>
+          <p>I love you so much!</p>
+          <p>
+            I wanted to let you know that you'll be getting Robux every single
+            month for an entire year as part of your birthday present.
+          </p>
+          <p>
+            Specifically, you'll be getting 2,200 Robux every month for 12
+            months!
+          </p>
+          <p>That's 2,200 x 12 = 26,400 Robux!!!</p>
+          <p>Don't spend it all in one obby!</p>
+          <p>Love,</p>
+          <p>Mommy and Daddy</p>
+        </div>
       </div>
     </>
   );
